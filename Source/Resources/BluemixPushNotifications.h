e209e0eb96e01b9ccbb6c1356131b64ff614ec03
Anthony Oliveri <oliveri@us.ibm.com>
2016-05-02 12:48:24 -0500

    Created XCode project

---
+++ Source/Resources/BluemixPushNotifications.h
@@ -0,0 +1,19 @@
+//
+//  BluemixPushNotifications.h
+//  BluemixPushNotifications
+//
+//  Created by Anthony Oliveri on 5/2/16.
+//  Copyright © 2016 IBM. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for BluemixPushNotifications.
+FOUNDATION_EXPORT double BluemixPushNotificationsVersionNumber;
+
+//! Project version string for BluemixPushNotifications.
+FOUNDATION_EXPORT const unsigned char BluemixPushNotificationsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BluemixPushNotifications/PublicHeader.h>
+
+
