6739ae58dfdb2453310a6d53cf789d4baeb8ea66
Taylor Franklin <tfranklin@us.ibm.com>
2017-03-28 16:00:46 -0500

    updated to swift 3.1

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
 	name: "BluemixPushNotifications",
     dependencies: [
-        .Package(url: "https://github.com/ibm-bluemix-mobile-services/bluemix-simple-http-client-swift.git", majorVersion: 0, minor: 5),
+        .Package(url: "https://github.com/ibm-bluemix-mobile-services/bluemix-simple-http-client-swift.git", majorVersion: 0, minor: 6),
         .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 15)
     ]
 )
