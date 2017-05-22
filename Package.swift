30cffd17a484b3e145d9b4f0fa6b4001bd0b37e4
jkingoliver <king@us.ibm.com>
2017-05-22 12:49:33 -0500

    upgrade swiftyjson to majorVersion: 16

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
 	name: "BluemixPushNotifications",
     dependencies: [
         .Package(url: "https://github.com/ibm-bluemix-mobile-services/bluemix-simple-http-client-swift.git", majorVersion: 0, minor: 6),
-        .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 15)
+        .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 16)
     ]
 )
