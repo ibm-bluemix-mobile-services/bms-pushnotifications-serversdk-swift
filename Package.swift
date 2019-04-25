9c3f6a36a6a4b018e5e91cc57463e79449fd2fc8
Ananth <agirijak@in.ibm.com>
2019-04-25 23:45:44 +0530

    package change

--- Package.swift
+++ Package.swift
@@ -1,8 +1,18 @@
-import PackageDescription
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
+import PackageDescription
 let package = Package(
-	name: "IBMPushNotifications",
+    name: "IBMPushNotifications",
     dependencies: [
-        .Package(url: "https://github.com/ibm-bluemix-mobile-services/bluemix-simple-http-client-swift.git", majorVersion: 0, minor: 9),
+        // Dependencies declare other packages that this package depends on.
+         .package(url: "https://github.com/ibm-bluemix-mobile-services/bluemix-simple-http-client-swift.git", .upToNextMajor(from: "0.9.0")),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "IBMPushNotifications",
+            dependencies: ["SimpleHttpClient"]),
     ]
 )
